18d79df517f45331c54bbb1f1c1ef39fda4dbe2d
courajs <courajs@users.noreply.github.com>
2021-03-19 20:08:59 +0000

    Update project name

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   let ENV = {
-    modulePrefix: 'ember-testy',
+    modulePrefix: 'ember-testy-new',
     environment,
     rootURL: process.env['EMBER_ROOT_URL'] ||'/',
     locationType: 'auto',
